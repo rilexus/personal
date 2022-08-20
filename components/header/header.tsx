ceca47d14a9bd5d0696c7792bc76a121a2d2fd21
rilexus <stanip@hotmail.de>
2022-08-20 16:22:55 +0200

    feat(header): publications

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,6 +3,7 @@ import { HeaderContent, HeaderWrapper, NavStyled } from "./header.style";
 import HeaderLogo from "./header-logo/header-logo";
 import styled from "styled-components";
 import Link from "next/link";
+import { useState } from "react";
 
 const Flex = styled.div`
   display: flex;
@@ -16,13 +17,110 @@ const LinksWrapper = styled.div`
   a {
     text-decoration: none;
     color: #2b2b2b;
-    margin-right: 20px;
+    &:hover {
+      color: blue;
+    }
   }
 `;
+
 export enum HeaderType {
   black = "black",
   white = "white"
 }
+
+const StyledSpacer = styled.div`
+  display: flex;
+  & > * {
+    margin-right: 20px;
+  }
+`;
+const Spacer = ({ children }: any) => {
+  return <StyledSpacer>{children}</StyledSpacer>;
+};
+
+const ListTitle = ({ children }: any) => {
+  return (
+    <div
+      style={{
+        fontSize: "0.8rem",
+        color: "#5d5d5d",
+        marginBottom: "0.5rem"
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Ul = styled.ul`
+  margin-bottom: 0.5rem;
+`;
+
+const Publications = ({ children }: any) => {
+  const [isOpen, setOpen] = useState(false);
+  const open = () => {
+    setOpen(true);
+  };
+  const close = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div
+      style={{
+        position: "relative"
+      }}
+    >
+      <div
+        style={{
+          cursor: "default"
+        }}
+        onMouseEnter={open}
+      >
+        Publications
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          width: "270px",
+          padding: "1rem",
+          display: isOpen ? "inherit" : "none"
+        }}
+        onMouseLeave={close}
+      >
+        <ListTitle>Articles</ListTitle>
+        <Ul>
+          <li>
+            <Link href={"https://rilexus.github.io/advanced-react-pattern/"}>
+              Advanced React Pattern
+            </Link>
+          </li>
+        </Ul>
+        <ListTitle>Packages</ListTitle>
+        <Ul>
+          <li>
+            <Link
+              href={"https://www.npmjs.com/package/react-transitions-library"}
+            >
+              react-transitions-library
+            </Link>
+          </li>
+          <li>
+            <Link href={"https://github.com/rilexus/react-microdata"}>
+              react-microdata
+            </Link>
+          </li>
+          <li>
+            <Link href={"https://www.npmjs.com/settings/nightfall-ui/packages"}>
+              Nightfall UI
+            </Link>
+          </li>
+        </Ul>
+      </div>
+    </div>
+  );
+};
+
 interface HeaderPropsI {}
 const Header = ({}: HeaderPropsI) => {
   return (
@@ -34,8 +132,11 @@ const Header = ({}: HeaderPropsI) => {
               <HeaderLogo />
             </div>
             <LinksWrapper>
-              <Link href={"/collaborations"}>Collaborations</Link>
-              <Link href={"/contact"}>Contact</Link>
+              <Spacer>
+                <Publications />
+                <Link href={"/collaborations"}>Collaborations</Link>
+                <Link href={"/contact"}>Contact</Link>
+              </Spacer>
             </LinksWrapper>
           </Flex>
         </HeaderContent>
